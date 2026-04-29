8173a6b89aea67acaff291e8655b587c149d8c6d
Jonathan <rockyroller2005@gmail.com>
2026-04-29 10:42:42 -0400

    teamleader class initialized.

--- FactoryWorkersInheritanceHierarchy/TeamLeader.cpp
+++ FactoryWorkersInheritanceHierarchy/TeamLeader.cpp
@@ -1,1 +1,23 @@
 #include "TeamLeader.h"
+
+#include <iostream>
+
+using namespace std;
+
+// Constructors
+
+TeamLeader::TeamLeader() : ProductionWorker(), monthlyBonus(0.0), requiredTrainingHours(0), attendedTrainingHours(0) {
+
+}
+
+TeamLeader::TeamLeader(string n, int num, string date, int shift, double rate, double bonus, int reqHours, int attHours) : ProductionWorker(n, num, date, shift, rate), monthlyBonus(bonus), requiredTrainingHours(reqHours), attendedTrainingHours(attHours) {
+
+}
+
+void TeamLeader::printTeamLeader() const {
+	printProductionWorker();
+
+	cout << "Monthly Bonus: $" << monthlyBonus << endl;
+	cout << "Required Training Hours: " << requiredTrainingHours << endl;
+	cout << "Attended Training Hours: " << attendedTrainingHours << endl;
+}
