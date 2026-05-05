1befd2b638285802088970906beacfac0838660c
Jonathan <rockyroller2005@gmail.com>
2026-05-05 17:25:04 -0400

    *Implementing Interactive system

--- FactoryWorkersInheritanceHierarchy/FactoryWorkersInheritanceHierarchy.cpp
+++ FactoryWorkersInheritanceHierarchy/FactoryWorkersInheritanceHierarchy.cpp
@@ -10,21 +10,35 @@ using namespace std;
 
 int main(){
 
-    // Assigning a production worker, shift supervisor and a team leader.
-    ProductionWorker productionworker("Michael Brown", 54, "1/13/2017", 1, 17.50);
-    ShiftSupervisor shiftsupervisor("Zachary Colins", 19, "9/22/2011", 47000, 4000);
-    TeamLeader teamleader("Lauren Hill", 8, "9/18/2009", 0, 31.75, 1300, 32, 43);
+    int choice;
 
-    productionworker.printProductionWorker();
+   // Interactive System
 
+    do {
 
-
-
-
-
+        cout << "Enter Production Worker[1]\n"
+            << "Exit [2]\n"
+            << "Choice: ";
+            cin >> choice;
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    } while (choice != 2);
 
 
 
